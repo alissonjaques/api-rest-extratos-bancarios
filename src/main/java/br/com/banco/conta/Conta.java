fcaa12a2dc594a8dfe198db5b617242a7ec7133e
Alisson Jaques <alissonjaquesrmq@gmail.com>
2022-12-24 23:37:45 -0300

    Adicionando a classes modelos Conta e Transferencia

---
+++ src/main/java/br/com/banco/conta/Conta.java
@@ -0,0 +1,14 @@
+package br.com.banco.conta;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "conta")
+public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_conta;
+    private String nome_responsavel;
+}
